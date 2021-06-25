5d9076b5a8f1b10e64f71f13de0681ea17b3e227
Navaz Alani <nalani@uwaterloo.ca>
2021-06-25 18:07:50 +0300

    render 2 squares with blending

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "setup.h"
 #include "errors.h"
 #include "shader/shader.h"
@@ -13,6 +16,7 @@
 #include "vertex_array/vertex_array.h"
 #include "renderer/renderer.h"
 #include "textures/texture.h"
+#include "chrono/chrono.h"
 
 int main(void) {
   if (setup() != 0) {
@@ -36,16 +40,22 @@ int main(void) {
   }
   glewExperimental=true; // Needed in core profile
   // Ensure we can capture the escape key being pressed below
-  glfwSwapInterval(1);
-  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+  //glfwSwapInterval(1);
+  //glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
   // buffer setup
 
-  float vertices[] = {
-    -0.5f, -0.5f, 0.0f, 0.0f,
-     0.5f, -0.5f, 1.0f, 0.0f,
-     0.5f,  0.5f, 1.0f, 1.0f,
-    -0.5f,  0.5f, 0.0f, 1.0f,
+  float rect1[] = {
+    -0.5f, -0.5f,
+     0.5f, -0.5f,
+     0.5f,  0.5f,
+    -0.5f,  0.5f
+  };
+  float rect2[] = {
+    -0.25f, -0.25f,
+     0.25f, -0.25f,
+     0.25f,  0.25f,
+    -0.25f,  0.25f
   };
 
   unsigned int indices[] = {
@@ -53,48 +63,75 @@ int main(void) {
     2, 3, 0,
   };
 
+
+  // setup blending
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
   GLClearError();
-  // setup vertex array
-  VertexArray va;
-  // setup vertex buffer + its layout
-  VertexBuffer vbuff{ vertices, 4*4*sizeof(float) };
+
+  // setup vertex arrays
+  VertexArray r1, r2;
+  // setup vertex buffer
+  VertexBuffer vbuff1{ rect1, 4*2*sizeof(float) },
+               vbuff2{ rect2, 4*2*sizeof(float) };
+  // setup layout
   BufferLayout layout;
   layout.PushField(2);
-  layout.PushField(2);
-  va.AddBuffer(vbuff, layout);
+  // bind the buffers to the separate vertex arrays
+  r1.AddBuffer(vbuff1, layout);
+  r2.AddBuffer(vbuff2, layout);
   GLCheckError();
+
   // setup index buffer
   IndexBuffer ib{ indices, 6 };
   // install shaders
   Shader sh{{
-    "resources/shaders/basic-vertex-shader.glsl",
-    "resources/shaders/basic-fragment-shader.glsl"
+    "resources/shaders/solid-color-vertex-shader.glsl",
+    "resources/shaders/solid-color-fragment-shader.glsl"
   }};
-  sh.Bind();
-  GLCheckError();
-  // setup textures
-  Texture t{ "resources/textures/OpenGL_logo.png" };
-  t.Bind();
   GLCheckError();
 
-  VectorUniform<int> textureUniform{ 1, 0 };
-  sh.setVectorUniform("u_Texture", &textureUniform);
+  // setup color uniforms
+  VectorUniform<float> rect1Color{ 4, 1.0f, 0.0f, 1.0f, 1.0f };
+  VectorUniform<float> rect2Color{ 4, 1.0f, 1.0f, 1.0f, 0.5f };
+  GLCheckError();
+
+
+  sh.Bind();
+  const char *colorUniform = "u_Color";
+  sh.setVectorUniform(colorUniform, &rect1Color);
+
   GLCheckError();
 
   // loop
   Renderer r;
 
+  unsigned int frameCount{ 0 };
+  Chrono timer;
+
   while (!glfwWindowShouldClose(window)) {
     r.Clear();
-    r.Draw(va, ib, sh);
+
+    sh.setVectorUniform(colorUniform, &rect1Color);
     GLCheckError();
+    r.Draw(r1, ib, sh);
+    GLCheckError();
+
+    ++frameCount;
+
+    sh.setVectorUniform(colorUniform, &rect2Color);
+    GLCheckError();
+    r.Draw(r2, ib, sh);
+    GLCheckError();
+
     // swap buffers and poll for events
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  timer.log("application time");
+  std::cout << frameCount<< " frames rendered\n";
 
   // cleanup
   glfwDestroyWindow(window);
